f4fd12af3129d3d0406b8495c3539db507f07129
Jordandize <675kostya@gmail.com>
2017-05-17 20:15:53 +0300

    init commit

---
+++ Assets/HeroFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+    public HeroRabit rabit;
+   
+	void Update () {
+        Transform rabitTransform = rabit.transform;
+        Transform cameraTransform = this.transform;
+       
+        Vector3 rabitPosition = rabitTransform.position;
+        Vector3 cameraPosition = cameraTransform.position;
+        
+        cameraPosition.x = rabitPosition.x;
+        cameraPosition.y = rabitPosition.y;
+        
+        cameraTransform.position = cameraPosition;
+    }
+}
---
+++ Assets/HeroRabit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabit : MonoBehaviour {
+
+    public float speed = 1;
+
+    Rigidbody2D myBody = null;
+    
+	void Start () {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+	
+	void Update () {
+		
+	}
+
+    void FixedUpdate() {
+        float speedKoeff = Input.GetAxis("Horizontal");
+
+        if(Mathf.Abs(speedKoeff) > 0) {
+            Vector2 newVelocity = myBody.velocity;
+            newVelocity.x = speedKoeff * speed;
+            myBody.velocity = newVelocity;
+        }
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if(speedKoeff < 0) {
+            sr.flipX = true;
+        } else if(speedKoeff > 0) {
+            sr.flipX = false;
+        }
+    }
+
+}
